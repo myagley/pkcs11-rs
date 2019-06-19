3bda887570a67941e63ad8b40ded002bc4c595db
Mike Yagley <myagley@gmail.com>
2019-06-18 17:24:26 -0700

    Update example

--- pkcs11/examples/sign.rs
+++ pkcs11/examples/sign.rs
@@ -9,22 +9,26 @@ fn main() {
 }
 
 fn run() -> Result<(), Error> {
-    // Initialize pkcs11 module
+    let key_bytes =
+        base64::decode("vSnr9DjnpfTCTjtG1LpFv4Ie476NBtOAyjUPzg4Y+H8=").expect("Invalid base64");
+
+    // Initialize pkcs11 module and login to session
     let module = ModuleBuilder::new()
         .path("/usr/local/lib/softhsm/libsofthsm2.so")
         .initialize()?;
     let mut session = module.session(595651617, SessionFlags::RW)?;
     session.login(UserType::User, "1234")?;
 
+    // Import the key
     let mut template = SecretKeyTemplate::new();
     template
         .key_type(KeyType::Sha256Hmac)
         .label("sas-key".to_string())
         // .is_token_object(true)
-        .value(
-            base64::decode("vSnr9DjnpfTCTjtG1LpFv4Ie476NBtOAyjUPzg4Y+H8=").expect("Invalid base64"),
-        );
+        .value(key_bytes);
     let key = session.create_object(&template)?;
+
+    // Sign and verify
     let signature = session.sign(&key, MechanismType::Sha256Hmac, "hello".as_bytes())?;
     let verified = session.verify(
         &key,
